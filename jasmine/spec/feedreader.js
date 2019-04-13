92eb160a970f674baba8c5eef8eed393f8cd5b8d
Naor <thehecticbyte@gmail.com>
2019-04-13 15:51:21 +0300

    test: Added a test for the feed loader - Added a test that checks if the feed loader loads feeds correctly

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,17 +9,13 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
+    /* This is our first test suite. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
+        /* A test that makes sure the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -58,40 +54,54 @@ $(function() {
 			expect(allContainNames).toBeTruthy();
         });
     });
-	//
-    /* TODO: Write a new test suite named "The menu" */
+    /* This is our second test suite. This suite is all about the Menu
+    * element. */
     describe('The menu', function() {
 		 /* A test that ensures the menu element is
-         * hidden by default. */
+         * hidden by default. 
          */
 		it('hidden by default', function() {
 			const isHidden = document.getElementsByTagName('body')[0].classList.contains('menu-hidden');
 			expect(isHidden).toBeTruthy();
 		});
 		  /* A test that ensures the menu changes
-          * visibility when the menu icon is clicked. */
+          * visibility when the menu icon is clicked.
+		  */
 		it('toggled when the nav-icon is clicked', function() {
 			const toggleIcon = document.getElementsByClassName('menu-icon-link')[0];
 			const bodyElement = document.getElementsByTagName('body')[0];
-				// First click, should make the navigation visible
-				toggleIcon.click(); 
-				expect(bodyElement.classList.contains('menu-hidden')).toBeFalsy();
-				// Second click, should make the navigation hidden
-				toggleIcon.click(); 
-				expect(bodyElement.classList.contains('menu-hidden')).toBeTruthy();
-
+			// First click, should make the navigation visible
+			toggleIcon.click(); 
+			expect(bodyElement.classList.contains('menu-hidden')).toBeFalsy();
+			// Second click, should make the navigation hidden
+			toggleIcon.click(); 
+			expect(bodyElement.classList.contains('menu-hidden')).toBeTruthy();
 		});
 	});
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+    describe('Initial Entries', function() {
+		const feed = document.getElementsByClassName('feed')[0];
+		let entry;
+		beforeEach(function(done) {
+			loadFeed(1, function(){
+				done();
+			});
+		
+		});
+		/* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+		 */
+		 it('feed loads the information correctly', function(done) {
+				let entry = document.getElementsByClassName('entry')[0];
+				const condition = feed.contains(entry);
+				expect(condition).toBeTruthy;
+				done();
+		 });
+	});
+	/*
 
+	*/
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
